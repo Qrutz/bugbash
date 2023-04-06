7f9d9e6767844601cab9a76875d79dd1adf74450
josandg <gussanjoba@student.gu.se>
2023-04-06 23:21:11 +0200

    nav styling & drag-n-drop setup

--- src/pages/projects/[id].tsx
+++ src/pages/projects/[id].tsx
@@ -1,7 +1,67 @@
-import { getSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import React from "react";
 import { api } from "~/utils/api";
+import {
+  RxDashboard,
+  RxCircle,
+  RxCardStackPlus,
+  RxAccessibility,
+} from "react-icons/rx";
+import { GoPlus } from "react-icons/go";
+import { BiDotsVertical } from "react-icons/bi";
+import { IconType } from "react-icons";
+
+type KanbanColumnProps = {
+  title: string;
+  children: React.ReactNode;
+  provided: any;
+  innerRef: any;
+};
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+
+const KanbanColumn: React.FC<KanbanColumnProps> = ({
+  title,
+  children,
+  innerRef,
+  provided,
+}) => {
+  return (
+    <div className="flex w-[30%] flex-col overflow-hidden rounded-lg bg-stone-900 ">
+      <div className="flex items-center justify-between px-4 py-2">
+        <h2 className="text-xl font-extrabold ">{title}</h2>
+        <div className="flex items-center space-x-2 text-xl">
+          <button className="  hover:text-gray-800 focus:outline-none">
+            <GoPlus />
+          </button>
+
+          <button className="text-2xl   hover:text-gray-800 focus:outline-none">
+            <BiDotsVertical />
+          </button>
+        </div>
+      </div>
+
+      <div
+        ref={innerRef}
+        {...provided.droppableProps}
+        className="flex-1 space-y-4 overflow-y-auto p-4"
+      >
+        {children}
+      </div>
+    </div>
+  );
+};
+
+function MenuTab(props: { name: string; icon: IconType }) {
+  return (
+    <span className="to-bg-neutral-800 flex w-full cursor-pointer justify-between rounded-md from-slate-800 to-slate-900 p-2 hover:bg-gradient-to-r">
+      <span className="flex items-center gap-2 text-lg">
+        <span className="text-xl"> {<props.icon />} </span>{" "}
+        <h1>{props.name}</h1>
+      </span>
+      <span className="w-[10%] rounded-md bg-red-600 text-center">4</span>
+    </span>
+  );
+}
 
 export default function Project() {
   const router = useRouter();
@@ -29,83 +89,91 @@ export default function Project() {
   if (projectStatus === "loading" || kanbanStatus === "loading") {
     return <div>Loading...</div>;
   }
-  console.log(getKanban);
 
   return (
-    <div className="container mx-auto bg-gray-800 shadow-sm shadow-black">
-      <div className="flex ">
-        <nav className="h-screen flex-1 border-r border-slate-700 bg-slate-900">
-          <div className="text-white-500 flex flex-col items-center justify-center gap-2 p-3">
-            <span>Dashboard</span>
-            <span>Projects</span>
-            <span>My Tasks</span>
-            <span>Settings</span>
-          </div>
-
-          <div className="text-white-500 flex flex-col items-center justify-center gap-2 p-3">
-            <span>Projects</span>
-            <span>p1</span>
-            <span>p2</span>
-            <span>p3</span>
-          </div>
-        </nav>
-        <main className="flex flex-[4] flex-col bg-slate-900 p-3">
-          <div className="flex justify-between">
-            <span>Projects / {getProject?.name} / Kanban </span>
-            <div className="flex space-x-1">
-              {getProject?.members.map((member) => {
-                return (
-                  <div className="rounded-full" key={member.id}>
-                    <img
-                      className="h-9 w-9 rounded-full"
-                      src={member.image || undefined}
-                      alt=""
-                    />
-                  </div>
-                );
-              })}
-              <button
-                className="rounded-md bg-gray-500 p-1"
-                onClick={handleAddMember}
-              >
-                Add Member
-              </button>
+    <DragDropContext onDragEnd={() => console.log("hello")}>
+      <div className="container mx-auto   shadow-sm shadow-black">
+        <div className="flex ">
+          <nav className="border-slate-00 h-screen flex-1 border-r bg-neutral-950">
+            <div className="text-white-500 flex flex-col items-center justify-center gap-4 p-4">
+              <MenuTab name="Dashboard" icon={RxDashboard} />
+              <MenuTab name="Projects" icon={RxCircle} />
+              <MenuTab name="My Tasks" icon={RxCardStackPlus} />
+              <MenuTab name="Settings" icon={RxAccessibility} />
             </div>
-          </div>
-          <h1 className="my-4 text-4xl font-bold">{getProject?.name}</h1>
-          {getKanban?.map((kanban) => {
-            return (
-              <div className="flex space-x-3" key={kanban.id}>
-                {kanban.columns.map((col) => {
-                  return (
-                    <div className="w-[25%] flex-col border" key={col.id}>
-                      <span className=" p-2">{col.name}</span>
-                      <div className="p-2">
-                        {col.cards.map((task) => {
-                          return (
-                            <div
-                              key={task.id}
-                              className="my-2 flex w-auto flex-col overflow-hidden rounded-md border bg-slate-800 p-2"
-                            >
-                              <span className="text-bold text-xl">
-                                {task.name}
-                              </span>
 
-                              <span className="text-gray-300">
-                                {task.description}
-                              </span>
-                            </div>
-                          );
-                        })}
-                      </div>
+            <div className="text-white-500 flex flex-col items-center justify-center gap-2 p-3">
+              <span>Projects</span>
+              <span>p1</span>
+              <span>p2</span>
+              <span>p3</span>
+            </div>
+          </nav>
+          <main className="flex flex-[4] flex-col bg-neutral-950 p-3">
+            <div className="flex justify-between">
+              <span>Projects / {getProject?.name} / Kanban </span>
+              <div className="flex space-x-1">
+                {getProject?.members.map((member) => {
+                  return (
+                    <div className="rounded-full" key={member.id}>
+                      <img
+                        className="h-9 w-9 rounded-full"
+                        src={member.image || undefined}
+                        alt=""
+                      />
                     </div>
                   );
                 })}
+                <button
+                  className="rounded-md bg-gray-500 p-1"
+                  onClick={handleAddMember}
+                >
+                  Add Member
+                </button>
               </div>
-            );
-          })}
-        </main>
+            </div>
+            <h1 className="my-4 text-4xl font-bold">{getProject?.name}</h1>
+            {getKanban?.map((kanban) => {
+              return (
+                <div className="flex space-x-3" key={kanban.id}>
+                  {kanban.columns.map((col) => {
+                    return (
+                      <Droppable droppableId="droppable-1">
+                        {(provided) => (
+                          <KanbanColumn
+                            title={col.name}
+                            key={col.id}
+                            provided={provided}
+                            innerRef={provided.innerRef}
+                          >
+                            {col.cards.map((task) => {
+                              return (
+                                <Draggable draggableId={task.id} index={0}>
+                                  {(provided) => (
+                                    <div
+                                      ref={provided.innerRef}
+                                      {...provided.draggableProps}
+                                      {...provided.dragHandleProps}
+                                      className="flex items-center justify-between rounded-md bg-stone-800 p-2"
+                                    >
+                                      <span>{task.name}</span>
+                                      <span>{task.description}</span>
+                                    </div>
+                                  )}
+                                </Draggable>
+                              );
+                            })}
+                          </KanbanColumn>
+                        )}
+                      </Droppable>
+                    );
+                  })}
+                </div>
+              );
+            })}
+          </main>
+        </div>
       </div>
-    </div>
+    </DragDropContext>
   );
 }
