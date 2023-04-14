dea3bd846092448376bb2508af2e53722880f59c
josandg <gussanjoba@student.gu.se>
2023-04-14 15:30:28 +0200

    edit task ui

--- src/components/TaskDialog.tsx
+++ src/components/TaskDialog.tsx
@@ -3,6 +3,8 @@ import { Dialog, Transition } from "@headlessui/react";
 import { useForm } from "react-hook-form";
 import type { SubmitHandler } from "react-hook-form";
 import { api } from "~/utils/api";
+import { BiPlus } from "react-icons/bi";
+import { RxCross1 } from "react-icons/rx";
 
 interface TaskDialogProps {
   isOpen: boolean;
@@ -68,52 +70,97 @@ export const TaskDialog = ({
         <div className="flex min-h-screen items-center justify-center">
           <Dialog.Overlay className="z-3 fixed inset-0 bg-black opacity-30" />
 
-          <div className="z-0 w-[25%]  rounded-lg bg-white p-4 text-black">
-            <h3 className="mb-4 text-lg font-bold">Edit Task</h3>
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <div className="mb-4">
-                <label
-                  htmlFor="name"
-                  className="mb-1 block text-sm font-semibold text-gray-700"
-                >
-                  Title
-                </label>
-                <input
-                  className="h-8 w-full  border border-gray-900 shadow-sm focus:border-neutral-800 focus:ring focus:ring-neutral-800 focus:ring-opacity-50 sm:text-sm"
-                  defaultValue={initialTaskName}
-                  {...register("name", { required: false })}
-                />
+          <div className="z-0 w-[62.5%] rounded-lg bg-white  p-4  text-black">
+            <div className="flex items-center justify-between ">
+              <h3 className=" text-lg font-bold">Edit Task</h3>
+              <button className="mb-3 cursor-pointer rounded-full p-1 hover:bg-gray-200">
+                <RxCross1 className="text-2xl" onClick={handleClose} />
+              </button>
+            </div>
+            <section className="flex gap-3 ">
+              <div className=" flex-[9] space-y-4  py-3">
+                <div className=" flex flex-col gap-1">
+                  <p className="text-gray-600">labels</p>
+                  <div className="flex gap-1 ">
+                    {" "}
+                    <span className="rounded-md bg-yellow-300 font-extralight hover:bg-yellow-600">
+                      <button className="p-1 text-sm "> UI/UX</button>
+                    </span>{" "}
+                    <span className="rounded-md bg-red-300 font-extralight hover:bg-red-600">
+                      <button className="p-1 text-sm "> Priority</button>
+                    </span>{" "}
+                    <span className="rounded-md bg-green-300 font-extralight hover:bg-green-600">
+                      <button className="p-1 text-sm "> Bug</button>
+                    </span>{" "}
+                    <span className="items-center rounded-md bg-gray-300 font-extralight hover:bg-gray-600">
+                      <button className=" p-1 text-center  text-lg ">
+                        {" "}
+                        <BiPlus />{" "}
+                      </button>
+                    </span>{" "}
+                  </div>
+                </div>
+
+                <main className="">
+                  <form onSubmit={handleSubmit(onSubmit)}>
+                    <div className="mb-4">
+                      <label
+                        htmlFor="name"
+                        className="mb-1 block text-lg font-semibold text-gray-700"
+                      >
+                        Title
+                      </label>
+                      <input
+                        placeholder="Task Name..."
+                        className=" w-full border  border-gray-900 p-1 text-lg shadow-sm      "
+                        defaultValue={initialTaskName}
+                        {...register("name", { required: false })}
+                      />
+                    </div>
+                    <div className="mb-4">
+                      <label
+                        htmlFor="description"
+                        className="mb-1 block text-lg  font-semibold  text-gray-700"
+                      >
+                        Description
+                      </label>
+                      <textarea
+                        defaultValue={initialTaskDescription}
+                        rows={3}
+                        placeholder="Task Description..."
+                        className="w-full rounded-md border   border-neutral-800 p-2 text-lg shadow-sm   "
+                        {...register("description", { required: false })}
+                      ></textarea>
+                    </div>
+                    <div className="mt-8">
+                      <button
+                        type="submit"
+                        onClick={onClose}
+                        className="inline-flex justify-center rounded-md border border-transparent bg-purple-500 px-4 py-2 text-sm font-medium text-white shadow-sm focus:outline-none focus:ring-2  focus:ring-offset-2 hover:bg-purple-600"
+                      >
+                        Save Changes
+                      </button>
+                      <button
+                        className="ml-4 inline-flex justify-center rounded-md border border-transparent bg-gray-200 px-4 py-2 text-sm font-medium text-gray-700 shadow-sm focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2 hover:bg-gray-300"
+                        onClick={handleClose}
+                      >
+                        Cancel
+                      </button>
+                    </div>
+                  </form>
+                </main>
               </div>
-              <div className="mb-4">
-                <label
-                  htmlFor="description"
-                  className="mb-1 block text-sm font-semibold  text-gray-700"
-                >
-                  Description
-                </label>
-                <textarea
-                  defaultValue={initialTaskDescription}
-                  rows={3}
-                  className="w-full border  border-neutral-800 text-lg shadow-sm focus:border-neutral-800  "
-                  {...register("description", { required: false })}
-                ></textarea>
-              </div>
-              <div className="mt-8">
-                <button
-                  type="submit"
-                  onClick={onClose}
-                  className="inline-flex justify-center rounded-md border border-transparent bg-blue-500 px-4 py-2 text-sm font-medium text-white shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 hover:bg-blue-600"
-                >
-                  Save Changes
-                </button>
-                <button
-                  className="ml-4 inline-flex justify-center rounded-md border border-transparent bg-gray-200 px-4 py-2 text-sm font-medium text-gray-700 shadow-sm focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2 hover:bg-gray-300"
-                  onClick={handleClose}
-                >
-                  Cancel
-                </button>
-              </div>
-            </form>
+
+              <nav className="flex-[2] flex-col space-y-2 py-3 ">
+                <h1 className="text-gray-500">Add to card</h1>
+                <span className="flex  cursor-pointer rounded-sm bg-gray-200 hover:bg-gray-300">
+                  <button className="px-1 py-1">Members</button>
+                </span>
+                <span className="flex  cursor-pointer rounded-sm bg-gray-200 hover:bg-gray-300">
+                  <button className="px-1 py-1">Labels</button>
+                </span>
+              </nav>
+            </section>
           </div>
         </div>
       </Dialog>
